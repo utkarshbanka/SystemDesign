b9007e8e3105791ecacb97bbf39974450fe56c72
utkarshbanka <utkarsh.k.keshari@gmail.com>
2023-11-04 16:08:59 +0530

    solid principal

---
+++ StaticsKeyword/Finalpi.java
@@ -0,0 +1,11 @@
+package SystemDesign.StaticsKeyword;
+
+public class Finalpi {
+    public final float pi = 3.14f;//we cannto modified this
+
+    final void fun1()
+    {
+        System.out.println("Hellow is system");
+    }
+
+}
